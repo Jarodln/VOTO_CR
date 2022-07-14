e7531494580b1ecb04f507edb47269da92f00f77
Jarod López <jarodln0299@gmail.com>
2022-07-13 21:22:11 -0600

    actualización de frm registro usuario

--- UI/frmRegistrarUsuario.Designer.cs
+++ UI/frmRegistrarUsuario.Designer.cs
@@ -32,6 +32,16 @@ namespace UI
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRegistrarUsuario));
             this.btnVolver = new System.Windows.Forms.Button();
             this.lblIconoUsuario = new System.Windows.Forms.Label();
+            this.lblCedula = new System.Windows.Forms.Label();
+            this.lblTipo = new System.Windows.Forms.Label();
+            this.lblContrasenna = new System.Windows.Forms.Label();
+            this.lblCorreo = new System.Windows.Forms.Label();
+            this.lblNombre = new System.Windows.Forms.Label();
+            this.lblApellido1 = new System.Windows.Forms.Label();
+            this.lblApellido2 = new System.Windows.Forms.Label();
+            this.lblFechaVencimiento = new System.Windows.Forms.Label();
+            this.lblPreguntaSeg = new System.Windows.Forms.Label();
+            this.lblRespuestaSeg = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // btnVolver
@@ -60,12 +70,122 @@ namespace UI
             this.lblIconoUsuario.Size = new System.Drawing.Size(120, 113);
             this.lblIconoUsuario.TabIndex = 1;
             // 
+            // lblCedula
+            // 
+            this.lblCedula.AutoSize = true;
+            this.lblCedula.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCedula.Location = new System.Drawing.Point(63, 155);
+            this.lblCedula.Name = "lblCedula";
+            this.lblCedula.Size = new System.Drawing.Size(61, 21);
+            this.lblCedula.TabIndex = 2;
+            this.lblCedula.Text = "Cédula:";
+            // 
+            // lblTipo
+            // 
+            this.lblTipo.AutoSize = true;
+            this.lblTipo.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTipo.Location = new System.Drawing.Point(63, 246);
+            this.lblTipo.Name = "lblTipo";
+            this.lblTipo.Size = new System.Drawing.Size(43, 21);
+            this.lblTipo.TabIndex = 3;
+            this.lblTipo.Text = "Tipo:";
+            // 
+            // lblContrasenna
+            // 
+            this.lblContrasenna.AutoSize = true;
+            this.lblContrasenna.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContrasenna.Location = new System.Drawing.Point(63, 326);
+            this.lblContrasenna.Name = "lblContrasenna";
+            this.lblContrasenna.Size = new System.Drawing.Size(92, 21);
+            this.lblContrasenna.TabIndex = 4;
+            this.lblContrasenna.Text = "Contraseña:";
+            // 
+            // lblCorreo
+            // 
+            this.lblCorreo.AutoSize = true;
+            this.lblCorreo.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCorreo.Location = new System.Drawing.Point(594, 246);
+            this.lblCorreo.Name = "lblCorreo";
+            this.lblCorreo.Size = new System.Drawing.Size(61, 21);
+            this.lblCorreo.TabIndex = 5;
+            this.lblCorreo.Text = "Correo:";
+            // 
+            // lblNombre
+            // 
+            this.lblNombre.AutoSize = true;
+            this.lblNombre.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNombre.Location = new System.Drawing.Point(228, 155);
+            this.lblNombre.Name = "lblNombre";
+            this.lblNombre.Size = new System.Drawing.Size(71, 21);
+            this.lblNombre.TabIndex = 6;
+            this.lblNombre.Text = "Nombre:";
+            // 
+            // lblApellido1
+            // 
+            this.lblApellido1.AutoSize = true;
+            this.lblApellido1.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApellido1.Location = new System.Drawing.Point(436, 155);
+            this.lblApellido1.Name = "lblApellido1";
+            this.lblApellido1.Size = new System.Drawing.Size(119, 21);
+            this.lblApellido1.TabIndex = 7;
+            this.lblApellido1.Text = "Primer apellido:";
+            // 
+            // lblApellido2
+            // 
+            this.lblApellido2.AutoSize = true;
+            this.lblApellido2.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblApellido2.Location = new System.Drawing.Point(594, 155);
+            this.lblApellido2.Name = "lblApellido2";
+            this.lblApellido2.Size = new System.Drawing.Size(134, 21);
+            this.lblApellido2.TabIndex = 8;
+            this.lblApellido2.Text = "Segundo apellido:";
+            // 
+            // lblFechaVencimiento
+            // 
+            this.lblFechaVencimiento.AutoSize = true;
+            this.lblFechaVencimiento.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblFechaVencimiento.Location = new System.Drawing.Point(288, 246);
+            this.lblFechaVencimiento.Name = "lblFechaVencimiento";
+            this.lblFechaVencimiento.Size = new System.Drawing.Size(190, 21);
+            this.lblFechaVencimiento.TabIndex = 9;
+            this.lblFechaVencimiento.Text = "Fecha vencimiento cédula:";
+            // 
+            // lblPreguntaSeg
+            // 
+            this.lblPreguntaSeg.AutoSize = true;
+            this.lblPreguntaSeg.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPreguntaSeg.Location = new System.Drawing.Point(308, 326);
+            this.lblPreguntaSeg.Name = "lblPreguntaSeg";
+            this.lblPreguntaSeg.Size = new System.Drawing.Size(170, 21);
+            this.lblPreguntaSeg.TabIndex = 10;
+            this.lblPreguntaSeg.Text = "Pregunta de seguridad:";
+            // 
+            // lblRespuestaSeg
+            // 
+            this.lblRespuestaSeg.AutoSize = true;
+            this.lblRespuestaSeg.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRespuestaSeg.Location = new System.Drawing.Point(594, 326);
+            this.lblRespuestaSeg.Name = "lblRespuestaSeg";
+            this.lblRespuestaSeg.Size = new System.Drawing.Size(180, 21);
+            this.lblRespuestaSeg.TabIndex = 11;
+            this.lblRespuestaSeg.Text = "Respuesta de Seguridad:";
+            // 
             // frmRegistrarUsuario
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblRespuestaSeg);
+            this.Controls.Add(this.lblPreguntaSeg);
+            this.Controls.Add(this.lblFechaVencimiento);
+            this.Controls.Add(this.lblApellido2);
+            this.Controls.Add(this.lblApellido1);
+            this.Controls.Add(this.lblNombre);
+            this.Controls.Add(this.lblCorreo);
+            this.Controls.Add(this.lblContrasenna);
+            this.Controls.Add(this.lblTipo);
+            this.Controls.Add(this.lblCedula);
             this.Controls.Add(this.lblIconoUsuario);
             this.Controls.Add(this.btnVolver);
             this.MaximizeBox = false;
@@ -82,5 +202,15 @@ namespace UI
 
         private System.Windows.Forms.Button btnVolver;
         private System.Windows.Forms.Label lblIconoUsuario;
+        private System.Windows.Forms.Label lblCedula;
+        private System.Windows.Forms.Label lblTipo;
+        private System.Windows.Forms.Label lblContrasenna;
+        private System.Windows.Forms.Label lblCorreo;
+        private System.Windows.Forms.Label lblNombre;
+        private System.Windows.Forms.Label lblApellido1;
+        private System.Windows.Forms.Label lblApellido2;
+        private System.Windows.Forms.Label lblFechaVencimiento;
+        private System.Windows.Forms.Label lblPreguntaSeg;
+        private System.Windows.Forms.Label lblRespuestaSeg;
     }
 }

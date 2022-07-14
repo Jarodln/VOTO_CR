836dea249530a415bc642be2a8e1831ca7f4687b
Jarod López <jarodln0299@gmail.com>
2022-07-13 21:02:22 -0600

    Agregando formulario de pregunta de seguridad

---
+++ UI/frmRegistrarPregunta.Designer.cs
@@ -0,0 +1,132 @@
+﻿
+namespace UI
+{
+    partial class frmRegistrarPregunta
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmRegistrarPregunta));
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblIconoPregunta = new System.Windows.Forms.Label();
+            this.lblPregunta = new System.Windows.Forms.Label();
+            this.txtPregunta = new System.Windows.Forms.TextBox();
+            this.btnGuardar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.button1.FlatAppearance.BorderColor = System.Drawing.Color.White;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.button1.Location = new System.Drawing.Point(1, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(99, 35);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "    Volver";
+            this.button1.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // lblIconoPregunta
+            // 
+            this.lblIconoPregunta.AutoSize = true;
+            this.lblIconoPregunta.Image = ((System.Drawing.Image)(resources.GetObject("lblIconoPregunta.Image")));
+            this.lblIconoPregunta.Location = new System.Drawing.Point(362, 68);
+            this.lblIconoPregunta.Name = "lblIconoPregunta";
+            this.lblIconoPregunta.Padding = new System.Windows.Forms.Padding(50);
+            this.lblIconoPregunta.Size = new System.Drawing.Size(100, 113);
+            this.lblIconoPregunta.TabIndex = 1;
+            // 
+            // lblPregunta
+            // 
+            this.lblPregunta.AutoSize = true;
+            this.lblPregunta.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPregunta.Image = ((System.Drawing.Image)(resources.GetObject("lblPregunta.Image")));
+            this.lblPregunta.ImageAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.lblPregunta.Location = new System.Drawing.Point(242, 201);
+            this.lblPregunta.Name = "lblPregunta";
+            this.lblPregunta.Padding = new System.Windows.Forms.Padding(0, 10, 0, 10);
+            this.lblPregunta.Size = new System.Drawing.Size(202, 41);
+            this.lblPregunta.TabIndex = 2;
+            this.lblPregunta.Text = "        Pregunta de seguridad:";
+            // 
+            // txtPregunta
+            // 
+            this.txtPregunta.BackColor = System.Drawing.SystemColors.Control;
+            this.txtPregunta.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPregunta.Location = new System.Drawing.Point(246, 236);
+            this.txtPregunta.Name = "txtPregunta";
+            this.txtPregunta.Size = new System.Drawing.Size(330, 29);
+            this.txtPregunta.TabIndex = 3;
+            // 
+            // btnGuardar
+            // 
+            this.btnGuardar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(65)))), ((int)(((byte)(253)))));
+            this.btnGuardar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.btnGuardar.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(11)))), ((int)(((byte)(65)))), ((int)(((byte)(253)))));
+            this.btnGuardar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnGuardar.Font = new System.Drawing.Font("Yu Gothic UI", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnGuardar.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.btnGuardar.Location = new System.Drawing.Point(352, 314);
+            this.btnGuardar.Name = "btnGuardar";
+            this.btnGuardar.Size = new System.Drawing.Size(122, 33);
+            this.btnGuardar.TabIndex = 4;
+            this.btnGuardar.Text = "Guardar";
+            this.btnGuardar.UseVisualStyleBackColor = false;
+            // 
+            // frmRegistrarPregunta
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGuardar);
+            this.Controls.Add(this.txtPregunta);
+            this.Controls.Add(this.lblPregunta);
+            this.Controls.Add(this.lblIconoPregunta);
+            this.Controls.Add(this.button1);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmRegistrarPregunta";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Registrar Pregunta Seguridad";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblIconoPregunta;
+        private System.Windows.Forms.Label lblPregunta;
+        private System.Windows.Forms.TextBox txtPregunta;
+        private System.Windows.Forms.Button btnGuardar;
+    }
+}
